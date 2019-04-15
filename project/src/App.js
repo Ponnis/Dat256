2a152d513dee10df69f302b235520cdd06452b3d
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2019-04-15 13:23:40 +0200

    New class for product

--- project/src/App.js
+++ project/src/App.js
@@ -1,46 +1,10 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import tomato from './tomato.jpg';
-import './App.css';
+import egg from './egg.jpg';
+import './App.css'
+import ShoppingItem from './Product'
 
-class ShoppingItem extends Component{
-  constructor(props){
-    super(props);
-    
-    this.state={
-      clicks: 0,
-      show: true,
-    };
-    this.vars={
-      src:tomato
-    };
-    
-  }
-  IncrementItem =()=>{
-    this.setState({clicks:this.state.clicks +1});
-  }
-  DecreaseItem =()=>{
-    this.setState({clicks:this.state.clicks -1});
-  }
-  ConfirmPurchase =()=>{
-    //TODO
-  }
-  SetSrc =(src)=>{
-    this.setState(this.vars.src=src);
-  }
-  render(){
-    return(
-      <div>
-      <button onClick={this.DecreaseItem}>-</button>
-      <img src={this.vars.src} alt={"kulk"} className="Tomato-logo"></img>
-      <button onClick={this.IncrementItem}>+</button>
-      {this.state.show ?<h2>{this.state.clicks}</h2>:''}
-      <button>köp</button>
-     </div>
-    );
-  }
-  
-}
 
 class Ppp extends Component {
   constructor(props){
@@ -53,9 +17,10 @@ class Ppp extends Component {
        
         <div className="App">
          <ShoppingItem></ShoppingItem>
+         <ShoppingItem></ShoppingItem>
         <p></p>
         <header className="App-header">
-          Rubrikjävl
+        
         </header>
       </div>
     );
---
+++ project/src/Product.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import tomato from './tomato.jpg';
+import egg from './egg.jpg';
+import './App.css'
+
+class ShoppingItem extends Component{
+    constructor(props){
+      super(props);
+      
+      this.state={
+        clicks: 0,
+        show: true,
+      };
+      this.vars={
+        src:tomato
+      };
+      
+    }
+    IncrementItem =()=>{
+      this.setState({clicks:this.state.clicks +1});
+    }
+    DecreaseItem =()=>{
+      this.setState({clicks:this.state.clicks -1});
+    }
+    ConfirmPurchase =()=>{
+      //TODO
+    }
+    SetSrc =(src)=>{
+      this.setState(this.vars.src=src);
+    }
+    render(){
+      return(
+        <div>
+        <button onClick={this.DecreaseItem}>-</button>
+        <img src={this.vars.src} alt={"kulk"} className="Tomato-logo"></img>
+        <button onClick={this.IncrementItem}>+</button>
+        {this.state.show ?<h2>{this.state.clicks}</h2>:''}
+        <button>köp</button>
+       </div>
+      );
+    }
+    
+  }
+  export default ShoppingItem;

af1b309e7aa0224288d6952a5781ba4ca218d8b6
sarahillstrom <sarahillstrom@hotmail.com>
2019-04-15 10:54:53 +0200

    designfix

---
+++ project/src/CloseIconPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CloseIconPage.css"
+
+const CloseIconPage = () => {
+return (
+<button type="button" className="" aria-label="Close">
+  <span aria-hidden="true">×</span>
+</button>
+);
+}
+
+export default CloseIconPage;
--- project/src/ConfirmationBox.js
+++ project/src/ConfirmationBox.js
@@ -1,10 +1,14 @@
 import React from "react"
 import './ConfirmationBox.css';
+import './CloseIconPage.js';
+import CloseIconPage from "./CloseIconPage.js";
 class ConfirmationBox extends React.Component {
     render() {
         const products = this.props.products.map(product => <h1>You purchased {product.amount} {product.item} from {product.producer}</h1>);
         return( 
         <div className= "ConfirmationBox">
+            <div className="CloseIconPage"><CloseIconPage/></div>
+            
             {products}
         </div>
         )

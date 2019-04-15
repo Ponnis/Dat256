438dc5a3055f0e91a4ad02c9963275513527f0d5
sarahillstrom <sarahillstrom@hotmail.com>
2019-04-15 09:49:13 +0200

    miniändring

--- project/src/TestPurchases.js
+++ project/src/TestPurchases.js
@@ -1,7 +1,7 @@
 const testPurchases = [
     {"producer": "Louises farm", "item": "tomatoes", "amount": 4},
-    {"producer": "Ellen", "item": "potatoes", "amount": 3},
-    {"producer": "Nils-Martin", "item": "eggs", "amount": 20},
-    {"producer": "Henrik", "item": "beef", "amount": 6}
+    {"producer": "Ellens vegetables", "item": "potatoes", "amount": 3},
+    {"producer": "Nils-Martins ranch", "item": "eggs", "amount": 20},
+    {"producer": "Henriks meat", "item": "beef", "amount": 6}
 ]
 export default testPurchases

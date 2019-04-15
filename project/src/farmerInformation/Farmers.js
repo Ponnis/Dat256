b147827a88d27d32aa30169920690c0b27c15db3
EllenWiderstrand <ellenwi@student.chalmers.se>
2019-04-15 11:32:38 +0200

    Made the new layout for farmerID, by using a function,  work.

--- project/src/farmerInformation/Farmers.js
+++ project/src/farmerInformation/Farmers.js
@@ -1,15 +1,21 @@
 const farmers = [{"id": 0, 
                 "name": "Bästa bondgården", 
                 "distance": 15, 
-                "description": "Det här är den absolut bästa bondgården. Alla våra produkter är ekologiska och riktigt goda."}];
+                "description": "Det här är den absolut bästa bondgården. Alla våra produkter är ekologiska och riktigt goda."},
+                {"id": 1, 
+                "name": "Djurens bondgård", 
+                "distance": 27, 
+                "description": "Det här är en bondgård där fokus ligger på att djuren ska må bra. Vi säljer ekologiskt kött från gris och lamm."}
+            ];
 
 
 function getFarmerById(id){
-    const f = farmers.map(farmer => {
-        if(farmer.id === id){
-            return farmer;
+    let f;
+    for(let i = 0; i < farmers.length; i++) {
+        if(farmers[i].id === id){
+            f = farmers[i]
         }
-    });
+    }
     return f;
 };
 

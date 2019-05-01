a9d12f1651cfd492802982a1c0d7b6a2e8fa01e2
EllenWiderstrand <ellenwi@student.chalmers.se>
2019-05-01 22:06:30 +0200

    Added icons for which type of produce the farmer sells.

--- project/src/farmerInformation/Farmers.js
+++ project/src/farmerInformation/Farmers.js
@@ -5,6 +5,11 @@
 const farmers = [{"id": 0, 
                 "name": "Bästa bondgården", 
                 "distance": 15, 
+                "types": [
+                    {"type": "egg"},
+                    {"type": "sheep"},
+                    {"type": "fish"}
+                ],
                 "description": "Det här är den absolut bästa bondgården. Alla våra produkter är ekologiska och riktigt goda.",
                 "products": [
                     {"name":"Tomatoes", "price":8},
@@ -14,6 +19,9 @@ const farmers = [{"id": 0,
                 {"id": 1, 
                 "name": "Djurens bondgård", 
                 "distance": 27, 
+                "types": [
+                    {"type": "fish"}
+                ],
                 "description": "Det här är en bondgård där fokus ligger på att djuren ska må bra. Vi säljer ekologiskt kött från gris och lamm.",
                 "products": [
                     {"name":"Potatoes", "price":10},

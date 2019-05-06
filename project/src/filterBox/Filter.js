de11c23584950439986f6579376f50e1b592a2d6
EllenWiderstrand <ellenwi@student.chalmers.se>
2019-05-06 13:59:43 +0200

    Added filter for categories.

--- project/src/filterBox/Filter.js
+++ project/src/filterBox/Filter.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 import './Filter.css'
+import Checkbox from 'rc-checkbox';
 
 class Filter extends Component {
     render() {
         return (
             <div className="FilterBox">
+                <div className="Header"> 
+                    Kategorier:
+                </div>
+                <label className="Label">
+                    <Checkbox>
+                    </Checkbox>
+                    &nbsp; <img src={require('../farmerInformation/images/icons/bread.png')} className="IconFilter"/>
+                </label>
+                <label className="Label">
+                    <Checkbox>
+                    </Checkbox>
+                    &nbsp; <img src={require('../farmerInformation/images/icons/egg.png')} className="IconFilter"/>
+                </label>
+                <label>
+                    <Checkbox>
+                    </Checkbox>
+                    &nbsp; <img src={require('../farmerInformation/images/icons/fish.png')} className="IconFilter"/>
+                </label>
             </div>
         )
     }

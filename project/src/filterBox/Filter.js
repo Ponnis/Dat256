d490a19d8b4e41a7e77e7ba9bc44ec6c9bed4ff7
henriklagergren <henriklagergren@hotmail.com>
2019-05-09 14:30:24 +0200

    Changed slider

--- project/src/filterBox/Filter.js
+++ project/src/filterBox/Filter.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Filter.css'
 import Checkbox from 'rc-checkbox';
 import StarRatings from 'react-star-ratings';
-import Slider from 'react-slider';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Filter extends Component {
 
@@ -11,26 +12,26 @@ class Filter extends Component {
         this.state = { rating: 1 }
     }
 
-    changeRating( newRating) {
+    changeRating(newRating) {
         this.setState({
-          rating: newRating
+            rating: newRating
         });
         this.props.onRatingClick(newRating);
-      }
+    }
 
     render() {
-        const categories = ["bread","egg","fish","fruit","meat","milk","sheep"];
+        const categories = ["bread", "egg", "fish", "fruit", "meat", "milk", "sheep"];
         return (
             <div className="FilterBox">
-                <div className="Header"> 
+                <div className="Header">
                     Kategorier:
                 </div>
-                {categories.map(cat => 
+                {categories.map(cat =>
                     <label className="Label">
                         <Checkbox className="CheckBox"
-                        onChange={(event) => this.props.onCategoryClick(cat,event.target.checked)}>
+                            onChange={(event) => this.props.onCategoryClick(cat, event.target.checked)}>
                         </Checkbox>
-                         &nbsp; <img src={require('../farmerInformation/images/icons/' + cat + '.png')} className="IconFilter"/>
+                        &nbsp; <img src={require('../farmerInformation/images/icons/' + cat + '.png')} className="IconFilter" />
                     </label>
                 )}
                 <div className="Header">
@@ -42,20 +43,13 @@ class Filter extends Component {
                     starHoverColor={"green"}
                     starDimension={'25px'}
                     starSpacing={'0px'}
-                    changeRating={(newRating,name) => this.changeRating(newRating,name)}
+                    changeRating={(newRating, name) => this.changeRating(newRating, name)}
                     name='rating'
                 />
                 <div className="Header">
                     Avstånd:
                 </div>
-                <Slider 
-                    className="Slider" 
-                    defaultValue={100}
-                    min={0} 
-                    step={5}
-                    >
-                    <div className="my-handle">o</div>
-                </Slider>
+                    <Slider defaultValue={10} />
             </div>
         )
     }

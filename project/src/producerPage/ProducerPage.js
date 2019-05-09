dfda2f1b6e98884c5fe460c0401ea14ac86cf1ca
RemainTheSame <34090420+RemainTheSame@users.noreply.github.com>
2019-05-09 13:21:41 +0200

    Producer Page

---
+++ project/src/producerPage/ProducerPage.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+class ProducerPage extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div></div>
+        )
+
+    }
+}
+
+export default ProducerPage

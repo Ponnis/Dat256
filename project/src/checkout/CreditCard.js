2970df70953631e827c3f8806e307d901d98223f
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2019-05-09 13:31:26 +0200

    creditCard component

---
+++ project/src/checkout/CreditCard.js
@@ -0,0 +1,17 @@
+import CreditCardInput from 'react-credit-card-input';
+import React from 'react'
+
+class CreditCardInput extends React.Component{
+
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}

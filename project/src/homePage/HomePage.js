7ebe2ce860d1d8ba97f51f2893503124cf255f9d
EllenWiderstrand <ellenwi@student.chalmers.se>
2019-05-13 11:25:22 +0200

    Added new searchbox for adress.

--- project/src/homePage/HomePage.js
+++ project/src/homePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./home.css";
-import Autocomplete from 'react-google-autocomplete';
+import Geosuggest from 'react-geosuggest';
 
 class HomePage extends React.Component{
     constructor(){
@@ -11,11 +11,36 @@ class HomePage extends React.Component{
         this.props.search("search")
     }
 
+    onSuggestSelect(suggest) {
+        alert(suggest);
+      }
+
     render(){
+        var fixtures = [
+            {label: 'Old Elbe Tunnel, Hamburg', location: {lat: 53.5459, lng: 9.966576}},
+            {label: 'Reeperbahn, Hamburg', location: {lat: 53.5495629, lng: 9.9625838}},
+            {label: 'Alster, Hamburg', location: {lat: 53.5610398, lng: 10.0259135}}
+          ];
         return(
             <div className={"home-container"}>
                 <h2>Skriv din adress här för att hitta gårdar nära dig:</h2>
                 <div><input className={"input-field"} type="text"/>
+                <div>
+        <Geosuggest
+          ref={el=>this._geoSuggest=el}
+          placeholder="Start typing!"
+        
+          fixtures={fixtures}
+          onSuggestSelect={this.onSuggestSelect}
+          
+          radius="20" />
+ 
+        
+        <button onClick={()=>this._geoSuggest.focus()}>Focus</button>
+    
+        <button onClick={()=>this._geoSuggest.clear()}>Clear</button>
+        <button onClick={()=>this._geoSuggest.selectSuggest()}>Search</button>
+      </div>
                 <button onClick={this.search} className={"SearchButton"}>sök</button>
                 </div>
             </div>

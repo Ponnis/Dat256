f41f4d5aefda20ecbf919ca0309d9908a4565998
henriklagergren <henriklagergren@hotmail.com>
2019-05-16 11:15:54 +0200

    Added button to find current location

--- project/src/homePage/HomePage.js
+++ project/src/homePage/HomePage.js
@@ -4,10 +4,12 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import "./home.css";
 import Geosuggest from 'react-geosuggest';
+import FindLocation from './FindLocation.js'
 
 class HomePage extends React.Component{
     constructor(){
         super()
+        this.state={onClick: false}
     }
     
     search=()=>{
@@ -29,8 +31,8 @@ class HomePage extends React.Component{
         return(
             <div className={"home-container"}>
                 <h2>Skriv din adress här för att hitta gårdar nära dig:</h2>
-                
-                <div>
+
+                <div style={{display: 'flex'}}>
                 <Geosuggest
                     className={"auto"}
                     fixtures={fixtures}
@@ -45,10 +47,11 @@ class HomePage extends React.Component{
                     maxFixtures={3}
                     placeholder={"Skriv in din adress"}
                     />
-                    <button onClick={this.search} className={"SearchButton"}>Hitta produkter nära dig</button>
+                    <img src={require('./myPosition.png')} onClick ={() => this.setState({onClick: !this.state.onClick})} style={{width: '25px', height: '25px', backgroundColor: 'white'}}/>
                 </div>
+                <button onClick={this.search} className ={"SearchButton"}>Hitta produkter nära dig!</button>
                 
-                
+                {this.state.onClick ? <FindLocation/>: <div/>}
             </div>
         )
     }
